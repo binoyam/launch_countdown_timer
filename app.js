8316a6147b00eed4ed2b02cc8a6fb38bbd390a82
binoyam <btechan@gmail.com>
2023-09-03 04:38:16 +0300

    show timer

--- app.js
+++ app.js
@@ -23,16 +23,26 @@ let previous;
 
 function showTimer() {
     timer.forEach((element) => {
-      const div = document.createElement("div");
-      div.classList.add(element.className);
-      div.innerHTML = `
+        const block = document.createElement("div");
+        block.classList.add(element.className);
+        block.innerHTML = `
         <div class="flip-card">
           <div class="top">00</div>
           <div class="bottom">00</div>
         </div>
         <p class="title">${element.label}</p>
       `;
-  
-      countdownContainer.append(div);
+
+        countdownContainer.append(block);
     });
-  }
+}
+showTimer()
+
+setInterval(() => {
+    const currentDate = new Date();
+    const timeBetweenDates = Math.floor((countToDate - currentDate) / 1000);
+    if (timeBetweenDates !== previous) {
+        flipAllCards(timeBetweenDates);
+    }
+    previous = timeBetweenDates;
+}, 250);
